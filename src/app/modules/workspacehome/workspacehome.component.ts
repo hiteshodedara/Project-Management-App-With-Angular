02df2effe58ab66ed21c3e0426265e30779dd09a
Odedara Hitesh <odedarahitesh1010@gmail.com>
2024-01-12 13:54:04 +0530

    make somechanges in workspace comp

--- src/app/modules/workspacehome/workspacehome.component.ts
+++ src/app/modules/workspacehome/workspacehome.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkspaceService } from 'src/app/services/workspace.service';
 
 @Component({
   selector: 'app-workspacehome',
   templateUrl: './workspacehome.component.html',
   styleUrls: ['./workspacehome.component.sass']
 })
-export class WorkspacehomeComponent {
+export class WorkspacehomeComponent implements OnInit{
 
+  workspaces:any;
+
+  constructor(private workspaceService:WorkspaceService) { }
+  
+  ngOnInit(): void {
+      this.workspaces=this.workspaceService.getallWorkspaces();
+
+  }
 }
--- src/app/services/workspace.service.ts
+++ src/app/services/workspace.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkspaceService {
 
+
+  workspace=[
+    {id:1,title:"rajkot team"},
+    {id:2,title:"ahemdabad team"},
+    {id:3,title:"hitesh team"},
+    {id:3,title:"hitesh team"},
+    {id:3,title:"hitesh team"},
+    {id:3,title:"hitesh team"},
+    {id:3,title:"hitesh team"},
+    {id:3,title:"hitesh team"},
+  ]
+
   constructor() { }
+
+  getallWorkspaces(){
+    return of(this.workspace)
+  }
 }
