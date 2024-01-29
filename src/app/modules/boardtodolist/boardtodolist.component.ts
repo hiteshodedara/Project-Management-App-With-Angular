ca4d4d44f18a422386f6be2df5ff171ea4eca93c
odedara hitesh <hiteshodedara1010@gmail.com>
2024-01-29 22:24:32 +0530

    infinite loop over now

--- src/app/modules/boardtodolist/boardtodolist.component.ts
+++ src/app/modules/boardtodolist/boardtodolist.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todolist } from 'src/app/models/todolist';
 
 @Component({
   selector: 'app-boardtodolist',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./boardtodolist.component.sass']
 })
 export class BoardtodolistComponent {
-
+  @Input() todolist!:Todolist;
 }
